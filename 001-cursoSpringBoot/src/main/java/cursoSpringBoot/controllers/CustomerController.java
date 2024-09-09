9f0922ad29489762fef0297fe76a67396461fa48
DeiMoS <ndelgado@hiberus.com>
2024-09-09 13:27:57 +0200

    getCostumerByUserName

--- 001-cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ 001-cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -1,8 +1,11 @@
 package cursoSpringBoot.controllers;
 
 import cursoSpringBoot.models.Customer;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,4 +24,24 @@ public class CustomerController {
     public List<Customer> getAllCustomers() {
         return customers;
     }
+
+    @GetMapping("/customers/{userName}")
+    public Customer getCustomerByUserName(@PathVariable String userName){
+        for(Customer c : customers){
+            if(c.getUserName().equals(userName)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    /* FORMA DE HACERLO CON UNA EXCEPCIÓN
+    @GetMapping("/customer/{userName}")
+    public Customer getCustomerByUserName(@PathVariable String userName) {
+        return customers.stream()
+                .filter(customer -> customer.getUserName().equals(userName))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
+    }
+     */
 }
