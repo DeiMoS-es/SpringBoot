e542651ea059e0d71f1b5f0aa5ebe37f89c89f1f
DeiMoS <ndelgado@hiberus.com>
2024-09-10 08:39:10 +0200

    deleteMapping

--- 001-cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ 001-cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -39,6 +39,29 @@ public class CustomerController {
         return customer;
     }
 
+    @PutMapping("/customers/{userName}")
+    public Customer updateCustomer(@PathVariable String userName, @RequestBody Customer customer){
+        for(Customer c : customers){
+            if(c.getUserName().equals(userName)){
+                c.setUserName(customer.getUserName());
+                c.setCustomerName(customer.getCustomerName());
+                c.setCustomerPassword(customer.getCustomerPassword());
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @DeleteMapping("/customers/{userName}")
+    public Customer deleteCustomer(@PathVariable String userName){
+        for(Customer c : customers){
+            if(c.getUserName().equals(userName)){
+                customers.remove(c);
+                return c;
+            }
+        }
+        return null;
+    }
     /* FORMA DE HACERLO CON UNA EXCEPCIÓN
     @GetMapping("/customer/{userName}")
     public Customer getCustomerByUserName(@PathVariable String userName) {
