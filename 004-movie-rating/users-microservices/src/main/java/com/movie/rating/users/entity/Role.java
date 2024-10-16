45e24c88b22172f8fa65b3bc29624914d6d40a71
DeiMoS <nagib.86@gmail.com>
2024-10-16 08:24:05 +0200

    create entity

---
+++ 004-movie-rating/users-microservices/src/main/java/com/movie/rating/users/entity/Role.java
@@ -0,0 +1,6 @@
+package com.movie.rating.users.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- 004-movie-rating/users-microservices/src/main/java/com/movie/rating/users/entity/User.java
+++ 004-movie-rating/users-microservices/src/main/java/com/movie/rating/users/entity/User.java
@@ -1,4 +1,30 @@
 package com.movie.rating.users.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Long userId;
+    @NotBlank
+    private String userName;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    private LocalDateTime createdAt;
+    private String role;
 }
