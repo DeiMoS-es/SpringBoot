bf61cd3762745c29b3f7e603c1bb6a5352b96752
DeiMoS <ndelgado@hiberus.com>
2024-12-19 17:58:24 +0100

    Recover 005-Movie-Rating from commit 4bb905875f43450bdaf981ebe5164e4fe896802f

---
+++ 005-Movie-Rating/src/main/java/com/movie_rating/domain/model/Movie.java
@@ -0,0 +1,59 @@
+package com.movie_rating.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("id")
+    private int movieId;
+    private boolean adult;
+    @JsonProperty("backdrop_path")
+    private String backdropPath;
+    @Transient
+    @JsonProperty("genre_ids")
+    private List<Integer> genreIds;
+    @ManyToMany
+    @JoinTable(
+            name = "movie_genre",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private List<Genre> genres;
+    @JsonProperty("original_language")
+    private String originalLanguage;
+    @JsonProperty("original_title")
+    private String originalTitle;
+    @Column(columnDefinition = "LONGTEXT")
+    private String overview;
+    private Double popularity;
+    @JsonProperty("poster_path")
+    private String posterPath;
+    @JsonProperty("release_date")
+    private String releaseDate;
+    private String title;
+    private boolean video;
+    @JsonProperty("vote_average")
+    private Double voteAverage;
+    @JsonProperty("vote_count")
+    private int voteCount;
+    @OneToMany(mappedBy = "movie")
+    private Set<Rating> ratings;
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = "https://image.tmdb.org/t/p/w220_and_h330_face" + posterPath;
+    }
+}
---
+++ 005-Movie-Rating/src/main/java/com/movie_rating/domain/model/Rating.java
@@ -0,0 +1,26 @@
+package com.movie_rating.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ratingId;
+    private int score;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+}
