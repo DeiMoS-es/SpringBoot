437c7f3a5a6c8f70aa2e36c2bb6aa57611bee948
DeiMoS <ndelgado@hiberus.com>
2024-12-23 18:42:50 +0100

    create genre entity

--- 006-movie-rating/src/main/java/com/movie_rating/movie/model/entity/Genre.java
+++ 006-movie-rating/src/main/java/com/movie_rating/movie/model/entity/Genre.java
@@ -1,4 +1,18 @@
 package com.movie_rating.movie.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(unique = true)
+    private int genreId;
 }
